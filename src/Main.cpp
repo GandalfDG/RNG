1aca5d56d10da3d15dc737e06c659f37465bbfd9
Jack Case <jackacase@gmail.com>
2020-04-28 18:28:15 +0000

    implemented and tested random number within range

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,5 +1,12 @@
+#include <iostream>
 #include "../include/GaloisRNG.hpp"
 
 int main() {
-    
+    GaloisRNG rng(4141996);
+
+    for(int i = 0; i < 100; i++) {
+        std::cout << rng.rand(1, 10) << std::endl;
+    }
+
+    return 0;
 }
--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,28 +1,44 @@
 #include "catch.hpp"
-#include "GaloisRNG.hpp"
+#include "../include/GaloisRNG.hpp"
+#include <cstdint>
 
 TEST_CASE("32-bit LFSR RNG is instantiated", "[LFSR]") {
-    GaloisRNG rng(123);
+    unsigned int seed = 123;
+    GaloisRNG rng(seed);
 
-    SECTION("default taps are the taps for maximal sequence") {
-        
-        REQUIRE( rng.getTaps().size() == 4 );
-        REQUIRE( rng.getTaps()[0] == 32 );
-        REQUIRE( rng.getTaps()[3] == 25 );    
+  SECTION("default taps are the taps for maximal sequence") {
 
-        REQUIRE( rng.getTapBinary() == 0xa3000000);
+    REQUIRE(rng.getTaps().size() == 4);
+    REQUIRE(rng.getTaps()[0] == 32);
+    REQUIRE(rng.getTaps()[3] == 25);
+
+    REQUIRE(rng.getTapBinary() == 0xa3000000);
     
     }
 
     SECTION("seed the RNG") {
 
-        REQUIRE( rng.getSeed() == 123);
+        REQUIRE( rng.getSeed() == seed);
 
     }
 
     SECTION("get first value out") {
 
-        REQUIRE(rng.rand() == 10);
+        REQUIRE(rng.rand() == 0xa300003d);
+    }
+
+    // SECTION("ensure maximal sequence", "[.slow]") {
+    //     long count = 1;
+    //     while(rng.rand() != seed) {
+    //         count++;
+    //     }
+    //     REQUIRE(count == UINT32_MAX);
+    // }
+
+    SECTION("get random value in range") {
+
+        REQUIRE(rng.rand(1u, 6u) == 6);
+
     }
 
 }
