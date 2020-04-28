9d0da47f9a3f1df9ac339d987f628dd0f131096b
Jack Case <jackacase@gmail.com>
2020-04-28 23:15:14 +0000

    attribution

--- src/FisherYates.cpp
+++ src/FisherYates.cpp
@@ -4,6 +4,7 @@
 FisherYates::FisherYates(GaloisRNG& rngref): rng(rngref) {}
 
 void FisherYates::shuffle(std::vector<std::uint32_t> &vec) {
+    // adapted from pseudocode found here: https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#The_modern_algorithm
     std::uint32_t randIdx = 0;
     std::uint32_t temp = 0;
 
